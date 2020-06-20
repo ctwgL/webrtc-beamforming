e72e3b47350f267f667d16065cbf963d9328208a
ctwgL <tingweichen@126.com>
2020-06-20 10:14:31 +0800

    build

---
+++ build/gflags/CMakeFiles/gflags_static.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ctw/webrtc_aec/wav-beamforming/gflags/src/gflags.cc" "/home/ctw/webrtc_aec/wav-beamforming/build/gflags/CMakeFiles/gflags_static.dir/src/gflags.cc.o"
+  "/home/ctw/webrtc_aec/wav-beamforming/gflags/src/gflags_completions.cc" "/home/ctw/webrtc_aec/wav-beamforming/build/gflags/CMakeFiles/gflags_static.dir/src/gflags_completions.cc.o"
+  "/home/ctw/webrtc_aec/wav-beamforming/gflags/src/gflags_reporting.cc" "/home/ctw/webrtc_aec/wav-beamforming/build/gflags/CMakeFiles/gflags_static.dir/src/gflags_reporting.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "gflags/include"
+  "../gflags/src"
+  "gflags/include/gflags"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
